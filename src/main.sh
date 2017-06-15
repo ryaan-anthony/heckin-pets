bb05eeb3932ee30122087443cdae9b9bb3e88849
Tulino, Ryan <rtulino@radial.com>
2017-06-15 13:30:39 -0400

    output basic info

--- src/main.sh
+++ src/main.sh
@@ -2,9 +2,20 @@
 
 function next
 {
+  if [[ ${#last} == 1 ]]; then
+    saved=$last
+    last=
+  fi
+  # TODO: create a grid for pet
   # TODO: maintain age / time
   # TODO: process user input
   # TODO: health meter
+  echo $saved
+  echo ${0: -1}
+  echo name: $NAME
+
+  read -n 4 -t 5 last
+  clear
 }
 
-while next; do sleep 0.1; done
+clear; while true; do next; done
