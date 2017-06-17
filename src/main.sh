f9ffe78ca536639afdc477aaf37becd301f65564
Tulino, Ryan <rtulino@radial.com>
2017-06-17 00:15:34 -0400

    add lots of indirection so i feel better about all the sloppy code im writing

--- src/main.sh
+++ src/main.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
+# Get the length of the hexdump signature
 function hexdump_length
 {
   echo $1 | hexdump | xargs | wc -c
 }
 
-function next
+# Describe the request flow
+function next_sequence
 {
-  hexdump_length=$(hexdump_length $last)
-  if [[ $((hexdump_length)) > 22 && ${#last} == 1 ]]; then
-    saved=$last
+  handle_last_input
+  display_current_sequence
+  prepare_next_input
+}
+
+# Listen for user input
+function prepare_next_input
+{
+  read -sn 4 -t 1 last_input
+  clear
+}
+
+# Consume user input
+function handle_last_input
+{
+  # Check user input for emoji
+  hexdump_length=$(hexdump_length $last_input)
+  if [[ $((hexdump_length)) > 22 && ${#last_input} == 1 ]]; then
+    saved=$last_input
     last=
   fi
+}
+
+# Display the emoji, grid, and stats
+function display_current_sequence
+{
   # TODO: create a grid for pet
   # TODO: maintain age / time
   # TODO: process user input
@@ -19,9 +42,10 @@ function next
   echo $saved
   echo ${0: -1}
   echo name: $NAME
-
-  read -sn 4 -t 1 last
-  clear
 }
 
-clear; while true; do next; done
+# Program start
+clear;
+while true; do
+  next_sequence
+done
