2614564fb69ef6adb60cc36ddaadcb366b54e1fd
Tulino, Ryan <rtulino@radial.com>
2017-06-17 01:19:04 -0400

    draw the grid

--- src/main.sh
+++ src/main.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
+grid_width=20 #character length
+grid_height=10 #line height
+refresh_rate=10 #seconds
+wall_character='#'
+floor_character='.'
+
 # Get the length of the hexdump signature
 function hexdump_length
 {
   echo $1 | hexdump | xargs | wc -c
+}
+
+function random_position
+{
+  max=$(($1 - 2))
+  echo $(( ( RANDOM % $max )  + 1 ))
+}
+
+function initialize
+{
+  max_x=$((grid_width -1))
+  max_y=$((grid_height -1))
+  position_x=$(random_position $max_x)
+  position_y=$(random_position $max_y)
+  emoji=${0: -1}
 }
 
 # Describe the request flow
@@ -17,7 +38,7 @@ function next_sequence
 # Listen for user input
 function prepare_next_input
 {
-  read -sn 4 -t 1 last_input
+  read -sn 4 -t $refresh_rate last_input
   clear
 }
 
@@ -27,25 +48,64 @@ function handle_last_input
   # Check user input for emoji
   hexdump_length=$(hexdump_length $last_input)
   if [[ $((hexdump_length)) > 22 && ${#last_input} == 1 ]]; then
-    saved=$last_input
-    last=
+    treat=$last_input
+    place_treat
+    last_input=
+  fi
+}
+
+function place_treat
+{
+  treat_x=$(random_position $max_x)
+  treat_y=$(random_position $max_y)
+  if [[ $treat_x == $position_x || $treat_y == $position_y ]]; then
+    place_treat
   fi
 }
 
 # Display the emoji, grid, and stats
 function display_current_sequence
 {
-  # TODO: create a grid for pet
-  # TODO: maintain age / time
-  # TODO: process user input
-  # TODO: health meter
-  echo $saved
-  echo ${0: -1}
+  animate_emoji
+  draw_grid
+  display_stats
+}
+
+function display_stats
+{
+  # TODO: display stats
   echo name: $NAME
 }
 
+function animate_emoji
+{
+  # TODO: animate emoji
+  animation=
+}
+
+function draw_grid
+{
+  for (( y=0; y < $grid_height; y++ )); do
+    column=
+    for (( x=0; x < $grid_width; x++ )); do
+      character=$floor_character
+      if [[ $position_x == $x && $position_y == $y ]]; then
+        character=$emoji
+      fi
+      if [[ $treat_x == $x && $treat_y == $y ]]; then
+        character=$treat
+      fi
+      if [[ $x == 0 || $x == $max_x || $y == 0 || $y == $max_y ]]; then
+        character=$wall_character
+      fi
+      column+=$character
+    done
+    echo $column
+  done
+}
 # Program start
 clear;
+initialize;
 while true; do
   next_sequence
 done
